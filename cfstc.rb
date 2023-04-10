900c07bd8e8dbaad6af64addcfadd2b97db7aed6
Mrered Cio <mrered@Mrered-MBA.local>
2023-04-10 10:46:43 +0800

    更新 cfstc.rb

--- cfstc.rb
+++ cfstc.rb
@@ -1,7 +1,7 @@
 class Cfstc < Formula
   desc "Select the best IP for your Mac on Cloudflare CDN"
-  homepage "https://github.com/XIU2/CloudflareSpeedTest"
-  url "https://github.com/Mrered/homebrew-cfstc/releases/download/v0.0.0/cfstc.tar.gz"
+  homepage "https://github.com/Mrered/cfstc"
+  url "https://github.com/Mrered/cfstc/releases/download/v0.0.0/cfstc.tar.gz"
   sha256 "3889abb6dc609bfc8f65760ca5b2df1a36f62e06f7462058153892867231b4c1"
   version "0.0.0"
   license "Apache-2.0"
